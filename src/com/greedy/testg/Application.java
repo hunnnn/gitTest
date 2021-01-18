44894333add1e97b69d68d5586bebd3c4b6b5e49
tj2 <tj2@DESKTOP-64T8B5K>
2021-01-18 11:17:22 +0900

    20210118_second commit
    띄어쓰기 하나 추가

--- src/com/greedy/testg/Application.java
+++ src/com/greedy/testg/Application.java
@@ -3,7 +3,7 @@ package com.greedy.testg;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		 System.out.println("Hello World!");
 
 	}
 
